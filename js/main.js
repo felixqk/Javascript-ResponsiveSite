67368c046de25f579fc413b3d240eb41ddaf5c5f
felixqk <felixqk@gmail.com>
2013-04-30 12:23:23 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// footer
+var footerString ="";
+footerString += "<div href=\"#\" class=\"footerimage\" style=\"position:relative;top:1px;left:30%;font-size:16px;display:inline;\">Follow us" 
+                    +"<a href=\"https://www.facebook.com/fastacash\" target=\"_blank\" style=\"margin-left:5px;\"><img src=\"images/footer-social-fb.png\" width=\"30\" height=\"30\"/></a>"
+                    +"<a href=\"https://twitter.com/fastacash\" target=\"_blank\" style=\"margin-left:5px;\"><img src=\"images/footer-social-twitter.png\" width=\"30\" height=\"30\"/></a>"
+                    +"<a href=\"https://twitter.com/fastacash\" target=\"_blank\" style=\"margin-left:5px;\"><img src=\"images/digicert.png\" width=\"83\" height=\"66\"  /></a> "                 
+                    +"</div><br /><div href=\"#\" class=\"footertext\" style=\"position:relative;top:10px;left:10%;font-size:10px;display:inline;\">fastacash &copy; 2012-2013.&nbsp;"
+                    +" All Rights Reserved.&nbsp; Patent Pending.</div> <div href=\"#\" class=\"footertext\" style=\"position:relative;top:10px;left:10%;font-size:10px;display:inline;\">"
+                    +" fastacash &trade; is a social (peer to peer) payment service offered by Fastacash Pte Ltd.<br /> Under current Singapore law, it is regarded as a stored value facility"
+                    +" which does not require the approval of the Monetary Authority of Singapore. Please read our <br /><a href=# target=\"_blank\">terms and conditions</a> carefully. "
+                    +" You can also refer to Monetary Authority of Singapore <a href=\"http://sg.sg/SVnNwJ\" target=\"_blank\">Stored Value Facilities Overview and Regulations</a> and "
+                    +" <a href=\"http://sg.sg/SO6srl\" target=\"_blank\">Stored Value Facility Guidelines</a>.</div>";           
